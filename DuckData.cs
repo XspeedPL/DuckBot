ff7bd7678b35d07696fba95dbd2cf3caf3971dbe
Xspeed <xspeedpl@windowslive.com>
2017-06-26 16:05:22 +0200

    Updated hardcoded user Ids to reflect ownership changes

--- DuckData.cs
+++ DuckData.cs
@@ -6,7 +6,7 @@ namespace DuckBot
 {
     internal class DuckData
     {
-        internal const ulong SuperUserId = 168285549088604160uL;
+        internal const ulong SuperUserId = 100615281310695424uL; // Xspeed
 
         internal static readonly DirectoryInfo SessionsDir = new DirectoryInfo("sessions");
         internal static readonly FileInfo StateLogFile = new FileInfo("State.log");
@@ -14,6 +14,6 @@ namespace DuckBot
         internal static readonly FileInfo TokenFile = new FileInfo("Token.cfg");
 
         internal readonly Dictionary<ulong, Session> ServerSessions = new Dictionary<ulong, Session>();
-        internal readonly List<ulong> AdvancedUsers = new List<ulong> { SuperUserId, 184688687391440904uL, 137237535700156416uL, 186295632229695488uL, 189088171043061760uL, 197026467446652928uL, 100615281310695424uL };
+        internal readonly List<ulong> AdvancedUsers = new List<ulong> { SuperUserId, 168285549088604160uL }; // Xspeed, EchoNex
     }
 }
